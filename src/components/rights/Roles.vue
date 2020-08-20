7b396624ea45f08f7d348239c3655adedcab6ac5
shanluzuode <3098387609@qq.com>
2020-08-20 23:04:41 +0800

    分配权限完成

--- src/components/rights/Roles.vue
+++ src/components/rights/Roles.vue
@@ -14,45 +14,21 @@
         <!-- 展开列开始-->
         <el-table-column width="100" align="center" type="expand">
           <template v-slot="scope">
-            <el-row
-              v-for="firstItem in scope.row.children"
-              :key="firstItem.id"
-              class="border-bottom center-in-col"
-            >
+            <el-row v-for="firstItem in scope.row.children" :key="firstItem.id" class="border-bottom center-in-col">
               <!-- 一级权限 -->
               <el-col :span="6">
-                <el-tag
-                  closable
-                  type="primary"
-                  @close="delRight(scope.row, firstItem)"
-                  >{{ firstItem.authName }}</el-tag
-                >
+                <el-tag closable type="primary" @close="delRight(scope.row, firstItem)" >{{ firstItem.authName }}</el-tag>
               </el-col>
               <!-- 二级和三级权限 -->
               <el-col :span="18">
-                <el-row
-                  v-for="secondItem in firstItem.children"
-                  :key="secondItem.id"
-                  class="center-in-col"
-                >
+                <el-row v-for="secondItem in firstItem.children" :key="secondItem.id" class="center-in-col">
                   <!-- 二级权限 -->
                   <el-col :span="6">
-                    <el-tag
-                      type="success"
-                      closable
-                      @close="delRight(scope.row, secondItem)"
-                      >{{ secondItem.authName }}</el-tag
-                    >
+                    <el-tag type="success" closable @close="delRight(scope.row, secondItem)" >{{ secondItem.authName }}</el-tag>
                   </el-col>
                   <!-- 三级权限 -->
                   <el-col :span="18">
-                    <el-tag
-                      type="warning"
-                      v-for="thirdItem in secondItem.children"
-                      :key="thirdItem.id"
-                      closable
-                      @close="delRight(scope.row, thirdItem)"
-                    >
+                    <el-tag type="warning" v-for="thirdItem in secondItem.children" :key="thirdItem.id" closable @close="delRight(scope.row, thirdItem)" >
                       {{ thirdItem.authName }}
                     </el-tag>
                   </el-col>
@@ -62,49 +38,19 @@
           </template>
         </el-table-column>
         <!-- 展开列结束-->
-        <el-table-column
-          width="100"
-          align="center"
-          type="index"
-          label="#"
-        ></el-table-column>
-        <el-table-column
-          prop="roleName"
-          label="角色名称"
-          width="300"
-          align="center"
-        ></el-table-column>
-        <el-table-column
-          prop="roleDesc"
-          label="角色描述"
-          width="300"
-          align="center"
-        ></el-table-column>
+        <el-table-column width="100" align="center" type="index" label="#"></el-table-column>
+        <el-table-column prop="roleName" label="角色名称" width="300" align="center"></el-table-column>
+        <el-table-column prop="roleDesc" label="角色描述" width="300" align="center"></el-table-column>
         <el-table-column label="操作" align="center">
           <template v-slot="scope">
-            <el-button type="primary" size="mini" icon="el-icon-edit"
-              >编辑</el-button
-            >
-            <el-button type="danger" size="mini" icon="el-icon-delete"
-              >删除</el-button
-            >
-            <el-button
-              type="warning"
-              size="mini"
-              icon="el-icon-setting"
-              @click="giveRight(scope.row)"
-              >分配权限</el-button
-            >
+            <el-button type="primary" size="mini" icon="el-icon-edit">编辑</el-button>
+            <el-button type="danger" size="mini" icon="el-icon-delete">删除</el-button>
+            <el-button type="warning" size="mini" icon="el-icon-setting" @click="giveRight(scope.row)">分配权限</el-button>
           </template>
         </el-table-column>
       </el-table>
       <!-- 分配权限对话框 -->
-      <el-dialog
-        title="分配权限"
-        :visible.sync="giveRightVisible"
-        width="40%"
-        @close="rightsDialogClose"
-      >
+      <el-dialog title="分配权限" :visible.sync="giveRightVisible" width="40%" @close="rightsDialogClose">
         <el-tree
           :data="rightsList"
           show-checkbox
@@ -112,13 +58,12 @@
           :props="{ children: 'children', label: 'authName' }"
           node-key="id"
           :default-checked-keys="defaultKeys"
+          ref="treeRef"
         >
         </el-tree>
         <span slot="footer" class="dialog-footer">
           <el-button @click="giveRightVisible = false">取 消</el-button>
-          <el-button type="primary" @click="giveRightVisible = false"
-            >确 定</el-button
-          >
+          <el-button type="primary" @click="giveRightSubmit">确 定</el-button>
         </span>
       </el-dialog>
     </el-card>
@@ -132,7 +77,8 @@ export default {
       rolesList: [],
       giveRightVisible: false, // 分配权限对话框显示与隐藏
       rightsList: [], // 所有权限列表
-      defaultKeys: [] // 存储所有已有三级权限的id
+      defaultKeys: [], // 存储所有已有三级权限的id
+      curRoleId: '' // 当前即将分配权限的id
     }
   },
   created() {
@@ -149,8 +95,6 @@ export default {
     },
     // 删除角色指定权限
     async delRight(role, right) {
-      console.log(role)
-      console.log(right)
       const confirmRes = await this.$confirm(
         `此操作将永久删除${role.roleName}的${right.authName}权限, 是否继续?`,
         '提示',
@@ -172,8 +116,8 @@ export default {
     },
     // 点击分配权限
     async giveRight(role) {
+      this.curRoleId = role.id // 保存要分配权限的角色id
       const res = await this.$http.get('rights/tree')
-      console.log(res)
       if (res.data.meta.status === 200) {
         this.rightsList = res.data.data
       } else {
@@ -184,7 +128,6 @@ export default {
     },
     // 获取所有需要默认选中的三级权限的递归函数
     getDefaultKeys(role) {
-      console.log('role:', role)
       if (role.children) {
         role.children.forEach(item => {
           this.getDefaultKeys(item)
@@ -196,6 +139,25 @@ export default {
     // 分配权限对话框关闭
     rightsDialogClose() {
       this.defaultKeys = [] // 需要重新将默认选中的权限置空，否则defaultKeys[]中的id越来越多
+    },
+    // 分配权限对话框提交
+    async giveRightSubmit() {
+      const allSelectKeys = this.$refs.treeRef.getCheckedKeys() // 拿到所有全选的keys
+      const halfSelectKeys = this.$refs.treeRef.getHalfCheckedKeys() // 拿到所有半选的keys
+      // console.log(allSelectKeys)
+      // console.log(halfSelectKeys)
+      const allKeys = [...allSelectKeys, ...halfSelectKeys].join(',')
+      // console.log(allKeys)
+      const res = await this.$http.post(`roles/${this.curRoleId}/rights`, {
+        rids: allKeys
+      })
+      if (res.data.meta.status === 200) {
+        this.$message.success('分配权限成功')
+        this.getRolesList()
+      } else {
+        this.$message.error('分配权限失败,' + res.data.meta.msg)
+      }
+      this.giveRightVisible = false
     }
   }
 }
