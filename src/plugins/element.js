548b8aa5fb845abb1cae25ed528fb28e6014f984
shanluzuode <3098387609@qq.com>
2020-08-21 11:28:19 +0800

    分配角色完成

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import {
   Button, Form, FormItem, Input, Message, Header, Container, Main, Aside, Row, Col, Avatar,
   Menu, Submenu, MenuItem, MenuItemGroup, Breadcrumb, BreadcrumbItem, Card, Table, TableColumn,
-  Tag, Switch, Pagination, Tooltip, Dialog, MessageBox
+  Tag, Switch, Pagination, Tooltip, Dialog, MessageBox, Select, Option
 } from 'element-ui'
 
 Vue.use(Button)
@@ -30,5 +30,7 @@ Vue.use(Switch)
 Vue.use(Pagination)
 Vue.use(Tooltip)
 Vue.use(Dialog)
+Vue.use(Select)
+Vue.use(Option)
 Vue.prototype.$message = Message // Message需要挂载到原型上使用
 Vue.prototype.$confirm = MessageBox.confirm
