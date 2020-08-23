5f1c32ccf294307d6dc96cf849e2185691e197a5
shanluzuode <3098387609@qq.com>
2020-08-23 21:49:42 +0800

    完成删除商品

--- src/components/goods/Goods.vue
+++ src/components/goods/Goods.vue
@@ -28,9 +28,9 @@
             <template v-slot="scope">{{scope.row.add_time | dateFormat}}</template>
         </el-table-column>
         <el-table-column label="操作" align="center" width="180">
-          <template>
+          <template v-slot="scope">
             <el-button icon="el-icon-edit" size="mini" type="primary"></el-button>
-            <el-button icon="el-icon-delete" size="mini" type="danger"></el-button>
+            <el-button icon="el-icon-delete" size="mini" type="danger" @click="delGood(scope.row.goods_id)"></el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -85,6 +85,22 @@ export default {
     handleCurrentChange(curPageNum) {
       this.queryParams.pagenum = curPageNum
       this.getGoodsList()
+    },
+    // 删除商品
+    async delGood(goodId) {
+      const confirmRes = await this.$confirm('此操作将永久删除该商品, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch(err => err)
+      if (confirmRes === 'cancel') return this.$message.info('已取消删除')
+      const res = await this.$http.delete(`goods/${goodId}`)
+      if (res.data.meta.status === 200) {
+        this.$message.success('删除成功')
+        this.getGoodsList()
+      } else {
+        this.$message.error('删除失败,' + res.data.meta.msg)
+      }
     }
   }
 }
